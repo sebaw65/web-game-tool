148b14dc243434e2b79f8ea63f4baa4953bac7a7
sebaw65 <sebaw65@gmail.com>
2023-10-15 16:16:46 +0200

    added model

--- src/Scene/Model/Model.tsx
+++ src/Scene/Model/Model.tsx
@@ -32,8 +32,8 @@ interface ModelProps {
 
 export const Model = forwardRef<THREE.Group, ModelProps>(
   ({ setAnimations }, sceneRef) => {
-    const { nodes, materials, animations } = useGLTF(
-      "/src/assets/char.glb"
+    const { animations, scene } = useGLTF(
+      "/src/assets/model.glb"
     ) as GLTFResult;
 
     useEffect(() => {
@@ -71,59 +71,7 @@ export const Model = forwardRef<THREE.Group, ModelProps>(
         rotation={rotation}
         scale={scale}
       >
-        <group name="Scene">
-          <group name="Scene_Root" scale={0.01}>
-            <skinnedMesh
-              name="CL"
-              geometry={nodes.CL.geometry}
-              material={materials.skin}
-              skeleton={nodes.CL.skeleton}
-            />
-            <skinnedMesh
-              name="FA_EA_1"
-              geometry={nodes.FA_EA_1.geometry}
-              material={materials.skin}
-              skeleton={nodes.FA_EA_1.skeleton}
-            />
-            <skinnedMesh
-              name="FA_Skin_1"
-              geometry={nodes.FA_Skin_1.geometry}
-              material={materials.skin}
-              skeleton={nodes.FA_Skin_1.skeleton}
-            />
-            <skinnedMesh
-              name="GL_1"
-              geometry={nodes.GL_1.geometry}
-              material={materials.skin}
-              skeleton={nodes.GL_1.skeleton}
-            />
-            <skinnedMesh
-              name="HR_1"
-              geometry={nodes.HR_1.geometry}
-              material={materials.skin}
-              skeleton={nodes.HR_1.skeleton}
-            />
-            <skinnedMesh
-              name="PA_1"
-              geometry={nodes.PA_1.geometry}
-              material={materials.skin}
-              skeleton={nodes.PA_1.skeleton}
-            />
-            <skinnedMesh
-              name="SH_1"
-              geometry={nodes.SH_1.geometry}
-              material={materials.skin}
-              skeleton={nodes.SH_1.skeleton}
-            />
-            <primitive object={nodes.Bip01} />
-            <primitive object={nodes.FA_Skin} />
-            <primitive object={nodes.CL_1} />
-            <primitive object={nodes.GL} />
-            <primitive object={nodes.SH} />
-            <primitive object={nodes.PA} />
-            <primitive object={nodes.FA_EA} />
-          </group>
-        </group>
+        <primitive object={scene} />
       </group>
     );
   }
